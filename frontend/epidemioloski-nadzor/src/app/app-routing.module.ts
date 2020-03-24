7d6da08aa02efad341faedc668f8089c7b8b5e78
markozivanov94 <markozivanov94@gmail.com>
2020-03-24 17:57:12 +0100

    Added role

--- frontend/epidemioloski-nadzor/src/app/app-routing.module.ts
+++ frontend/epidemioloski-nadzor/src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RoleGuard } from './auth/role.guard';
 import { PatientsComponent } from './patients/patients.component';
 import { PatientComponent } from './patients/patient/patient.component';
 import { RegisterComponent } from './users/register/register.component';
 import { LoginComponent } from './users/login/login.component';
 
 const routes: Routes = [
-  { path: 'register', component: RegisterComponent },
   { path: '', component: LoginComponent },
-  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  // { 
+  //   path: 'register', component: RegisterComponent, 
+  //   canActivate: [RoleGuard], data: { expectedRoles: ['ROLE_ADMINISTRATOR'] } 
+  // },
   { path: 'patients', component: PatientsComponent },
   { path: 'register-patient', component: PatientComponent },
   { path: 'edit-patient/:jmbg', component: PatientComponent }
