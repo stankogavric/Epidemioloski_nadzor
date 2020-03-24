9535c8bc3b60f9a6971c5184d8c9513b3839ed61
Vujos <vujos.petrovic@gmail.com>
2020-03-24 19:14:19 +0100

    Fixed registration on frontend

--- frontend/epidemioloski-nadzor/src/app/users/register/register.component.ts
+++ frontend/epidemioloski-nadzor/src/app/users/register/register.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { FormErrorService } from '../../shared/formError.service';
 import { UserService } from '../user.service';
 import { User } from '../user.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -11,11 +12,11 @@ import { User } from '../user.model';
 })
 export class RegisterComponent implements OnInit {
 
-  hide = true;
   public form: FormGroup;
   public user: User = new User();
+  message: string = "";
 
-  constructor(private userService: UserService, public readonly formError: FormErrorService, private fb: FormBuilder) { }
+  constructor(private userService: UserService, public readonly formError: FormErrorService, private fb: FormBuilder,  private router: Router) { }
 
   ngOnInit() {
     this.form = this.fb.group({
@@ -28,11 +29,20 @@ export class RegisterComponent implements OnInit {
 
   onRegister() {
     this.user.personalInfo.firstname = this.form.value.firstName;
-    this.user.personalInfo.firstname = this.form.value.lastName;
+    this.user.personalInfo.lastname = this.form.value.lastName;
     this.user.personalInfo.phone = this.form.value.phone;
     this.user.pin = this.form.value.pin;
 
-    this.userService.add(this.user);
+    this.userService.add(this.user).subscribe(
+      () => {
+        this.message = "";
+        this.router.navigate(['/']);
+      },
+      (e) => {
+        if (e.status == 409){
+          this.message = "Korisnik sa tim telefonom je vec registrovan";
+        }
+      });
   }
 
 }
